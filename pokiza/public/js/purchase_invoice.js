f19b478571a538b4424bd070c9dd054ddfdb952c
Munisa33 <munisabax2002@gmail.com>
2026-08-27 22:22:04 +0500

    deful time in PI/PE/SI

--- pokiza/public/js/purchase_invoice.js
+++ pokiza/public/js/purchase_invoice.js
@@ -5,6 +5,14 @@ frappe.ui.form.on("Purchase Invoice", {
         setPurchaseItemQuery(frm);
     },
 
+    onload(frm) {
+        // Yangi hujjatda vaqt default 09:00 (qo'lda o'zgartirish mumkin)
+        if (frm.is_new() && !frm.doc.amended_from) {
+            frm.set_value("set_posting_time", 1);
+            frm.set_value("posting_time", "09:00:00");
+        }
+    },
+
     refresh(frm) {
         setPurchaseItemQuery(frm);
     },
--- pokiza/public/js/sales_invoice.js
+++ pokiza/public/js/sales_invoice.js
@@ -3,6 +3,14 @@ frappe.ui.form.on("Sales Invoice", {
         setSalesItemQuery(frm);
     },
 
+    onload(frm) {
+        // Yangi hujjatda vaqt default 15:00 (qo'lda o'zgartirish mumkin)
+        if (frm.is_new() && !frm.doc.amended_from) {
+            frm.set_value("set_posting_time", 1);
+            frm.set_value("posting_time", "15:00:00");
+        }
+    },
+
     refresh(frm) {
         setSalesItemQuery(frm);
     },
